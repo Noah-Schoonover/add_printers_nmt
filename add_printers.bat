a9dc377b6126b8fc34248333596ac5c45345385d
Noah Schoonover <noah@skoonie.com>
2022-03-07 22:20:25 -0700

    print error msg if printer doesn't exist

--- add_printers.bat
+++ add_printers.bat
@@ -6,9 +6,12 @@ set printers=
 for /f "tokens=*" %%a in (printers.cfg) do (
 	set p=%%a
 	IF /I "!p:~0,1!" NEQ "#" (
-		echo adding printer: \\Acad-PrintBox\!p!
 		IF EXIST \\Acad-PrintBox\!p! (
+			echo adding printer: \\Acad-PrintBox\!p!
 			start rundll32 printui.dll PrintUIEntry /in /n\\Acad-PrintBox\!p!
+		)
+		IF NOT EXIST \\Acad-PrintBox\!p! (
+			echo printer does not exist: \\Acad-PrintBox\!p!
 		)
 	)
 )
