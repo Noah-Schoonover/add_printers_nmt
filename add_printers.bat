3ee801ac72e8204b65f2d976687c2cb00e234ed5
Noah Schoonover <noah@skoonie.com>
2022-03-07 21:56:06 -0700

    add multithreading

--- add_printers.bat
+++ add_printers.bat
@@ -7,7 +7,7 @@ for /f "tokens=*" %%a in (printers.cfg) do (
 	set p=%%a
 	IF /I "!p:~0,1!" NEQ "#" (
 		echo adding printer: \\Acad-PrintBox\!p!
-		rundll32 printui.dll PrintUIEntry /in /n\\Acad-PrintBox\!p!
+		start rundll32 printui.dll PrintUIEntry /in /n\\Acad-PrintBox\!p!
 	)
 )
 pause
