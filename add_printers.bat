52b1397bca1f58e2ee9e868c973f2a96e36efea1
Noah Schoonover <noah@skoonie.com>
2022-03-07 22:10:10 -0700

    only add printers that exist

--- add_printers.bat
+++ add_printers.bat
@@ -7,7 +7,9 @@ for /f "tokens=*" %%a in (printers.cfg) do (
 	set p=%%a
 	IF /I "!p:~0,1!" NEQ "#" (
 		echo adding printer: \\Acad-PrintBox\!p!
-		start rundll32 printui.dll PrintUIEntry /in /n\\Acad-PrintBox\!p!
+		IF EXIST \\Acad-PrintBox\!p! (
+			start rundll32 printui.dll PrintUIEntry /in /n\\Acad-PrintBox\!p!
+		)
 	)
 )
 pause
