81da86e5899d048f726755851447cb20a6a923b6
Noah Schoonover <noah@skoonie.com>
2022-03-09 14:14:30 -0700

    Update add_printers.bat

--- add_printers.bat
+++ add_printers.bat
@@ -1,18 +1,13 @@
 CLS
 @echo off
 @setlocal enableextensions enabledelayedexpansion
-
+cd /d %~dp0
 set printers=
-for /f "tokens=*" %%a in (printers.cfg) do (
+for /f "tokens=*" %%a in (%~dp0printers.cfg) do (
 	set p=%%a
 	IF /I "!p:~0,1!" NEQ "#" (
-		IF EXIST \\Acad-PrintBox\!p! (
-			echo adding printer: \\Acad-PrintBox\!p!
-			start rundll32 printui.dll PrintUIEntry /in /n\\Acad-PrintBox\!p!
-		)
-		IF NOT EXIST \\Acad-PrintBox\!p! (
-			echo printer does not exist: \\Acad-PrintBox\!p!
-		)
+		echo adding printer: \\Acad-PrintBox\!p!
+		start rundll32 printui.dll PrintUIEntry /in /n\\Acad-PrintBox\!p!
 	)
 )
 pause
